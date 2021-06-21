8cf106a8c2224ee5f5bae7130fa833f45dc8c20e
Batman <gtaverne@student.42.fr>
2021-06-21 21:57:08 +0200

    On avance sur la config

---
+++ 5-onycroit/srcs/requirements/wordpress/tools/setup-wp.sh
@@ -0,0 +1,28 @@
+echo "On rentre dans le .sh"
+
+cd
+#Remplacera les valeurs fournies par le .env dans le wp-config
+#On teste bien a chaque fois l'existence du fichier afin de pouvoir gerer les reboots
+[ -f /tmp/wp-config.php ] && sed -i -e "s|MYSQL_DATABASE|'$MYSQL_DATABASE'|g" /tmp/wp-config.php
+[ -f /tmp/wp-config.php ] && sed -i -e "s|MYSQL_USER|'$MYSQL_USER'|g" /tmp/wp-config.php
+[ -f /tmp/wp-config.php ] && sed -i -e "s|MYSQL_PASSWORD|'$MYSQL_PASSWORD'|g" /tmp/wp-config.php
+[ -f /tmp/wp-config.php ] && sed -i -e "s|WORDPRESS_DB_HOST|'$WORDPRESS_DB_HOST'|g" /tmp/wp-config.php
+
+sed -i -e "s|;daemonize = yes|daemonize = no|g" /etc/php/7.3/fpm/php-fpm.conf
+
+if [ ! -d "/var/www/wordpress" ]; then
+echo "AUCUN SITE TROUVE"
+echo "Installation d'un wordpress vierge"
+mv /wordpress /var/www/
+else
+echo "Un site est pre-configure"
+rm -rf /wordpress
+fi
+
+[ -f /tmp/wp-config.php ] && mv /tmp/wp-config.php /var/www/wordpress/wp-config.php
+
+
+chmod -R 755 /var/www/
+mkdir -p /run/php/
+
+exec /usr/sbin/php-fpm7.3
