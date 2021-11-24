32b77a0caad64c5af20835e95a96df15b7e87912
Batman <gtaverne@student.42.fr>
2021-11-24 18:52:04 +0100

    On a peut etre un progres

--- 4-onavance/requirements/nginx/tools/setupnginx.sh
+++
@@ -1,5 +0,0 @@
-nginx -g "daemon off;"
-
-sleep 1
-cd ~
-echo -n 'ok ' >>/www/boume
--- 5-onycroit copy/srcs/requirements/bonus/jupyter/tools/jupyterstart.sh
+++
@@ -1,8 +0,0 @@
-echo "Launching jupyter"
-
-jupyter notebook --allow-root --generate-config
-[ -f /tmp/jupyter_notebook_config.py ] && mv /tmp/jupyter_notebook_config.py /root/.jupyter/jupyter_notebook_config.py
-
-#jupyter notebook --no-browser --allow-root --ip 0.0.0.0
-
-jupyter notebook
