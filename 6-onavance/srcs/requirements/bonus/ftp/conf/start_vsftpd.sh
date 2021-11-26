51fda864dc337bc46a781960f9ea2d5b36130618
Batman <gtaverne@student.42.fr>
2021-11-26 22:29:45 +0100

    Ca ressemble a un ftp qui fonctionne

--- 6-onavance/srcs/requirements/bonus/ftp/conf/start_vsftpd.sh
+++ 6-onavance/srcs/requirements/bonus/ftp/conf/start_vsftpd.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-echo "$FTP_USER"
-echo "$FTP_PASS"
 
-{ echo "$FTP_PASS"; echo "$FTP_PASS"; } | adduser $FTP_USER
+# { echo "$FTP_PASS"; echo "$FTP_PASS"; } | adduser $FTP_USER
+{ echo "$FTP_PASS"; echo "$FTP_PASS"; } | adduser --home /home/ftp $FTP_USER
 
-chgrp -R $FTP_USER /home/$FTP_USER
+
+chgrp -R $FTP_USER /home/ftp/
+echo "*********Here are the files the FTP can access**********" && \
+cd /home/ftp/ && ls
 chmod -R 755 /home/ftp/
-chmod -R g-w /home/$FTP_USER
 
 vsftpd /etc/vsftpd/vsftpd.conf
