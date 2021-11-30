0588a66a9825c8a0efef85434f2af097c61f4971
Batman <gtaverne@student.42.fr>
2021-11-30 23:22:37 +0100

    On avance sur Mariadb

--- 0-nobonus/srcs/requirements/mariadb/tools/my_maria-setup.sh
+++ 0-nobonus/srcs/requirements/mariadb/tools/my_maria-setup.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-service mysql start
-
-#mysqld_safe --init-file=/tmp/mysql-init.sql &
+# service mysql start
+# mysqld_safe --init-file=/tmp/mysql-init.sql &
 
 
 sleep 10
@@ -13,21 +12,13 @@ echo "*********************************"
 
 dbreuse=$(echo "SHOW DATABASES" | mysql -u root -p'$MYSQL_ROOT_PASSWORD'| grep $MYSQL_DATABASE | wc -l)
 
-echo "Do we have a db ?"
+echo "Do we alread have a $MYSQL_DATABASE db ?"
 if [ $dbreuse == 1 ]; then
 echo "Yaisse"
 
 else
-echo "Non"
-# service mysql start
-# echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci" | mysql -u root
-# echo "CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'" | mysql -u root
-# echo "GRANT ALL ON $MYSQL_DATABASE .* TO '$MYSQL_USER'@'%'" | mysql -u root
-# echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';" | mysql -u root
-# echo "flush privileges;" | mysql -u root -p'$MYSQL_ROOT_PASSWORD'
+echo "Non, on a un probleme"
 fi
 
-sleep infinity
-
-exec /usr/sbin/mysqld -u mysql
+exec /usr/sbin/mysqld -u mysql &
 #--defaults-file=/etc/mysql/my.cnf
